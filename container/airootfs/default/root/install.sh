13db96449dc620e9feb1c3ce03752945ac2b3f32
spareproject <christopher.thornbury@gmail.com>
2015-09-06 08:28:35 +0000

    ermmmmmm twerked?

--- container/airootfs/default/root/install.sh
+++ container/airootfs/default/root/install.sh
@@ -12,12 +12,14 @@ user=$(shuf -i 1000-60000 -n 1)
 group=$(shuf -i 1000-60000 -n 1)
 groupadd --gid ${group} group
 useradd -m --uid ${user} -g group -s /bin/bash user;
-gpg --homedir /home/user/gnupg --passphrase-fd 0 --gen-key --batch /etc/gnupg/batch.user <<< "" 2>/dev/null
-gpg --homedir /home/user/gnupg --output /home/user/user.public --export user  2>/dev/null
-ssh-keygen -t rsa -b 4096 -f "/home/user/ssh/id_rsa" -N ""
-ssh-keygen -t rsa -b 4096 -f "/home/user/sshd/ssh_host_rsa_key" -N ""
-gpg --homedir /home/user/gnupg -e /home/user/ssh/id_rsa 2>/dev/null;#rm /home/user/ssh/id_rsa
-gpg --homedir /home/user/gnupg -e /home/user/sshd/ssh_host_rsa_key 2>/dev/null;#rm /home/user/sshd/ssh_host_rsa_key
+gpg --homedir /home/user/gnupg/user --passphrase-fd 0 --gen-key --batch /etc/gnupg/batch.user <<< "" 2>/dev/null
+gpg --homedir /home/user/gnupg/user --output /home/user/gnupg/user.public --export user  2>/dev/null
+ssh-keygen -t rsa -b 4096 -f "/home/user/openssh/ssh/id_rsa" -N ""
+ssh-keygen -t rsa -b 4096 -f "/home/user/openssh/sshd/ssh_host_rsa_key" -N ""
+gpg --homedir /home/user/gnupg/user -e /home/user/openssh/ssh/id_rsa 2>/dev/null;
+#rm /home/user/openssh/ssh/id_rsa
+gpg --homedir /home/user/gnupg/user -e /home/user/openssh/sshd/ssh_host_rsa_key 2>/dev/null;
+#rm /home/user/openssh/sshd/ssh_host_rsa_key
 chown -R user:group /home/user;
 chmod -R 700 /home/user
 passwd -l user
